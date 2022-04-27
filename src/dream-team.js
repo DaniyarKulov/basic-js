7fbca1078a759a41216eaf61cfca43fbcd54691e
Daniyar <kulovdaniyar77@gmail.com>
2022-04-27 23:36:43 +0600

    feat: hanoi-tower

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,25 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
- * @return {String | Boolean} name of the team or false
- * in case of incorrect members
- *
- * @example
- * 
- * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
- * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
- *
- */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+//  Create name of dream team based on the names of its members
+
+// @param { Array } members names of the members
+// @return { String | Boolean} name of the team or false
+// 	in case of incorrect members
+
+// @example
+
+
+
+
+function createDreamTeam(members) {
+	let a = members.filter(a => typeof a[0].toUpperCase() == 'string').sort((a, b) => a - b).trim()
+
+	return a
 }
 
 module.exports = {
-  createDreamTeam
+	createDreamTeam
 };
+createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max'])
+createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null])
--- src/what-season.js
+++ src/what-season.js
@@ -12,10 +12,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+	//   throw new NotImplementedError('Not implemented');
+	// remove line with error and write your code here
 }
 
 module.exports = {
-  getSeason
+	getSeason
 };
